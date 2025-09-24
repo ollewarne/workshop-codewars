343f4d3087e3656d5aaae4a6fa80510ccad00dac
ollewarne <209520777+ollewarne@users.noreply.github.com>
2025-09-24 10:32:08 +0200

    add pseudo code solution

---
+++ main.js
@@ -0,0 +1,8 @@
+// GOAL: calculate the area if it is a square or perimeter if it is a rectangle
+// INPUTS: the length and the width of the polygon. Both are positive integers.
+// RETURN: if the length and width are equal, return area. If they are not equal return perimeter.
+// STEP 1: Create a function with parameters of length and width
+// STEP 2: if length and width are equal. Return length*width (area).
+// STEP 3: if length and widht are NOT equal. Return 2(length+width) (perimeter).
+// solved! Woohoo!
+// Pseudo code for https://www.codewars.com/kata/5ab6538b379d20ad880000ab
