8782f5e3db68ac8f2f0afa24a2b82563281d86a2
ollewarne <209520777+ollewarne@users.noreply.github.com>
2025-09-24 11:59:10 +0200

    add another

---
+++ nr2.js
@@ -0,0 +1,8 @@
+// goal: find out if a string is in alphabetical order
+// input: a string of lower case letters
+// return: true if its in alphabetical order or false if not
+// step 1: sort the string in alphabetical order
+// step 2: check if the sorted string is the same as the initial string
+// step 3: if the string is the same. Return tummen upp
+// step 4: if not the same. Return tummen ner
+// https://www.codewars.com/kata/5a8059b1fd577709860000f6
