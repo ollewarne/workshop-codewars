d521d5ff057fa6c60549c1c274d1dfd45adefe0d
ollewarne <209520777+ollewarne@users.noreply.github.com>
2025-09-24 10:45:30 +0200

    spelling mistake

--- main.js
+++ main.js
@@ -3,6 +3,6 @@
 // RETURN: if the length and width are equal, return area. If they are not equal return perimeter.
 // STEP 1: Create a function with parameters of length and width
 // STEP 2: if length and width are equal. Return length*width (area).
-// STEP 3: if length and widht are NOT equal. Return 2(length+width) (perimeter).
+// STEP 3: if length and width are NOT equal. Return 2(length+width) (perimeter).
 // solved! Woohoo!
 // Pseudo code for https://www.codewars.com/kata/5ab6538b379d20ad880000ab
